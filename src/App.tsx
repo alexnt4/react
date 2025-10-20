263aeb9e4c7ed4983d27eae04e98f593e0693212
Alex <kend1123ue@gmail.com>
2025-10-20 17:07:30 -0500

    feat: add custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { useFetch } from "./hooks";
 //import { Button } from "./components";
 
+const url = "https://api.example.com/data";
+
+interface Data {
+  name: string;
+  lastName: string;
+  age: number;
+}
+
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  const consoleLoader = (loadingValue: boolean) => {
-    setLoading(loadingValue);
-    console.info(loading);
-  };
-
-  const fechtData = async () => {
-    consoleLoader(true);
-    try {
-      const response = await fetch("https://api.example.com/data");
-
-      if (!response.ok) {
-        throw new Error("Error al obtener datos");
-      }
-      const jsonData = await response.json();
-
-      setData(jsonData);
-    } catch (err) {
-      setError(err as string);
-    } finally {
-      consoleLoader(false);
-    }
-  };
-
-  useEffect(() => {
-    fechtData();
-  }, []);
+  const { data, error, loading } = useFetch<Data>(url);
 
   if (loading) {
     return <div>Cargando...</div>;
   }
 
   if (error) {
-    return <div>UPS! Hay un error: {error}</div>;
+    return <div>UPS! Hay un error: {error.message}</div>;
   }
 
   return <div>{JSON.stringify(data)}</div>;
